775cf6ed5701dc85bcb3d4277e7b4fb0379bb1a4
zidan_1013 <80787878+zidan4@users.noreply.github.com>
2026-02-04 21:29:20 +0300

    Create typeof.r

---
+++ typeof.r
@@ -0,0 +1,23 @@
+typeof(1)
+
+typeof("Hello")
+
+typeof(Inf)
+
+typeof(NaN)
+
+typeof(TRUE)
+
+typeof(as.integer(1))
+
+10 == 10
+ 
+ 10 != 10
+
+ as.logical(0)
+
+ as.logical(1)
+
+ typeof(as.numeric("5"))
+
+typeof(as.numeric(TRUE))
